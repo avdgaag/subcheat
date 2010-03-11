0032015eb5572725d9ae09fac184f523b26dae55
Arjan van der Gaag <info@agwebdesign.nl>
2010-03-11 17:13:50 +0100

    Moved exceptions into module

--- lib/subcheat.rb
+++ lib/subcheat.rb
@@ -4,6 +4,19 @@
 end
 
 module Subcheat
+  # General-purpose program-specific exception.
+  Exception        = Class.new(Exception)
+
+  # Raised when looking for a custom command that does not exist.
+  NoSuchCommand    = Class.new(Exception)
+
+  # Raised when trying to have subversion work on a directory
+  # that is not a working copy.
+  NotAWorkingCopy  = Class.new(Exception)
+
+  # General-purpose exception that commands can raise to halt execution.
+  CommandException = Class.new(Exception)
+
   # Report the version number from /VERSION
   def version
     File.read(File.join(File.dirname(__FILE__), *%w{.. VERSION}))
